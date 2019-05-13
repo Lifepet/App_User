03ba9d2cddb7f0b1f758032820cd87d844c44d66
eggbee <ty39563956@gmail.com>
2019-05-13 22:37:44 +0900

    [UPDATE] DetailBindingAdapter

--- app/src/main/java/com/example/app_user/ui/detail_adopt/DetailAdoptBindingAdapter.kt
+++ app/src/main/java/com/example/app_user/ui/detail_adopt/DetailAdoptBindingAdapter.kt
@@ -6,7 +6,7 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
-@BindingAdapter("imageUrl","error")
-    fun setImageUrl(view:ImageView,uri:String,error:Drawable){
+@BindingAdapter("image","errorImage")
+    fun setImageUrl(view:ImageView,uri:String?,error:Drawable){
     Glide.with(view.context).load(uri).apply(RequestOptions().override(150, 150)).into(view)
 }
